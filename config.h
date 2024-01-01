0b28a6a66de41237642cf1871586b0763a341ea1
dre <davionellison02@gmail.com>
2024-01-01 09:13:24 -0500

    added more status info

--- config.h
+++ config.h
@@ -65,5 +65,12 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
+	{ disk_perc, "[HDD %s%%] ", "/mnt/hdd/"},
+        { disk_perc, "[Root Part %s%%] ", "/"},
+        { netspeed_tx, "[NetSend %sB/s] ", "enp9s0"},
+        { netspeed_rx, "[NetRec %sB/s] ", "enp9s0"},
+        { run_command, "[GPU %s] ", "bash ~/misc-scripts/gpu-util.sh"},
+        { cpu_perc, "[CPU %s%%] ", NULL},
+        { ram_perc, "[RAM %s%%] ", NULL},
 	{ datetime, "%s",           "%F %T" },
 };
